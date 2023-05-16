6105782b3db59cbbfe414c8940b5d9e275742803
Andy Fiddaman <illumos@fiddaman.net>
2023-05-16 19:03:10 +0000

    15666 legacy libipadm consumers can fail after 13316
    Change-ID: I6fa595e02de708f658d2b72341163e0d55130c34

--- usr/src/lib/libipadm/common/ipadm_addr.c
+++ usr/src/lib/libipadm/common/ipadm_addr.c
@@ -2155,18 +2155,10 @@ i_ipadm_lookupadd_addrobj(ipadm_handle_t iph, ipadm_addrobj_t ipaddr)
 	rvalp = &rval;
 	err = ipadm_door_call(iph, &larg, sizeof (larg), (void **)&rvalp,
 	    sizeof (rval), B_FALSE);
-	if (err == 0) {
-		/*
-		 * Save daemon-generated state.  Unconditionally copy
-		 * the `lnum` from the daemon, and copy `aobjname' if
-		 * we did not give a name.
-		 */
-		ipaddr->ipadm_lifnum = rval.ir_lnum;
-		if (ipaddr->ipadm_aobjname[0] == '\0') {
-			(void) strlcpy(ipaddr->ipadm_aobjname,
-			    rval.ir_aobjname,
-			    sizeof (ipaddr->ipadm_aobjname));
-		}
+	if (err == 0 && ipaddr->ipadm_aobjname[0] == '\0') {
+		/* copy the daemon generated `aobjname' into `ipadddr' */
+		(void) strlcpy(ipaddr->ipadm_aobjname, rval.ir_aobjname,
+		    sizeof (ipaddr->ipadm_aobjname));
 	}
 	if (err == EEXIST)
 		return (IPADM_ADDROBJ_EXISTS);
@@ -2651,7 +2643,6 @@ ipadm_create_addr(ipadm_handle_t iph, ipadm_addrobj_t addr, uint32_t flags)
 	sa_family_t		other_af;
 	boolean_t		created_af = B_FALSE;
 	boolean_t		created_other_af = B_FALSE;
-	ipadm_addr_type_t	type;
 	char			*ifname = addr->ipadm_ifname;
 	boolean_t		legacy = (iph->iph_flags & IPH_LEGACY);
 	boolean_t		aobjfound = B_FALSE;
@@ -2671,7 +2662,7 @@ ipadm_create_addr(ipadm_handle_t iph, ipadm_addrobj_t addr, uint32_t flags)
 	if (status != IPADM_SUCCESS)
 		return (status);
 	/*
-	 * For Legacy case, check if an addrobj already exists for the
+	 * For the Legacy case, check if an addrobj already exists for the
 	 * given logical interface name. If one does not exist,
 	 * a default name will be generated and added to the daemon's
 	 * aobjmap.
@@ -2718,6 +2709,13 @@ ipadm_create_addr(ipadm_handle_t iph, ipadm_addrobj_t addr, uint32_t flags)
 		status = i_ipadm_lookupadd_addrobj(iph, addr);
 		if (status != IPADM_SUCCESS)
 			return (status);
+		/*
+		 * Record that the address object in the daemon is currently a
+		 * placeholder. This is important to know so that address
+		 * objects are properly cleaned up after an error.
+		 */
+		if (!legacy)
+			addr->ipadm_lifnum = -1;
 	}
 
 	is_6to4 = i_ipadm_is_6to4(iph, ifname);
@@ -2816,8 +2814,7 @@ ipadm_create_addr(ipadm_handle_t iph, ipadm_addrobj_t addr, uint32_t flags)
 	}
 
 	/* Create the address. */
-	type = addr->ipadm_atype;
-	switch (type) {
+	switch (addr->ipadm_atype) {
 	case IPADM_ADDR_STATIC:
 		status = i_ipadm_create_addr(iph, addr, flags);
 		break;
@@ -2838,33 +2835,28 @@ ipadm_create_addr(ipadm_handle_t iph, ipadm_addrobj_t addr, uint32_t flags)
 		    gifname);
 	}
 
+	if (status == IPADM_SUCCESS)
+		return (status);
+
+fail:
+	if (status == IPADM_DHCP_IPC_TIMEOUT)
+		return (status);
+
 	/*
 	 * If address was not created successfully, unplumb the interface
-	 * if it was plumbed implicitly in this function and remove the
-	 * addrobj created by the ipmgmtd daemon as a placeholder.
-	 * If IPH_LEGACY is set, then remove the addrobj only if it was
-	 * created in this function.
+	 * if it was plumbed implicitly in this function.
 	 */
-fail:
-	if (status != IPADM_DHCP_IPC_TIMEOUT &&
-	    status != IPADM_SUCCESS) {
-		if (!legacy) {
-			if (created_af || created_other_af) {
-				if (created_af) {
-					(void) i_ipadm_delete_if(iph, ifname,
-					    af, flags);
-				}
-				if (created_other_af) {
-					(void) i_ipadm_delete_if(iph, ifname,
-					    other_af, flags);
-				}
-			} else {
-				(void) i_ipadm_delete_addrobj(iph, addr, flags);
-			}
-		} else if (!aobjfound) {
-			(void) i_ipadm_delete_addrobj(iph, addr, flags);
-		}
-	}
+	if (created_af)
+		(void) i_ipadm_delete_if(iph, ifname, af, flags);
+	if (created_other_af)
+		(void) i_ipadm_delete_if(iph, ifname, other_af, flags);
+
+	/*
+	 * Remove the addrobj created by the ipmgmtd daemon.  If IPH_LEGACY is
+	 * set, then do so only if it was created in this function.
+	 */
+	if (!legacy || !aobjfound)
+		(void) i_ipadm_delete_addrobj(iph, addr, flags);
 
 	return (status);
 }
