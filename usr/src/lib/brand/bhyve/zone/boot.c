89bc69f73a4aa13f3b196c5c26abd0559a4afa37
neirac <cneira@edgecast.io>
2025-07-11 15:37:48 +0000

    change comment style

--- usr/src/lib/brand/bhyve/zone/boot.c
+++ usr/src/lib/brand/bhyve/zone/boot.c
@@ -456,12 +456,14 @@ add_nets(int *argc, char **argv)
 			nextpcifn++;
 		}
 
-		// if you look in bhyve(8) you will see that
-		// one can append "[,network-backend-options]"
-		// If this vnic has been marked to allow promiscuity
-		// by using allow_mac_spoofing=true, we will want to
-		// add ",promiscphys=true"
-		// All other network backend options are by default true.
+		/*
+		 * if you look in bhyve(8) you will see that
+		 * one can append "[,network-backend-options]"
+		 * If this vnic has been marked to allow promiscuity
+		 * by using allow_mac_spoofing=true, we will want to
+		 * add ",promiscphys=true"
+		 * All other network backend options are by default true.
+		 */
 		if (snprintf(slotconf, sizeof (slotconf),
 		    "%d:%d,virtio-net-viona,%s%s", PCI_SLOT_NICS, pcifn, net,
 		    is_env_true("net", net, "allow_mac_spoofing") ?
